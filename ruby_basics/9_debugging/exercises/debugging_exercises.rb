31311545185467bbf9974b9febd405ef5fec3a9f
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-03-18 11:44:35 -0400

    Fix glitch on isogram? method
    The split method on isogram? was splitting based on words instead of individual letters. After placing some puts and p, I debugged the issue and fixed the glitch.

--- ruby_basics/9_debugging/exercises/debugging_exercises.rb
+++ ruby_basics/9_debugging/exercises/debugging_exercises.rb
@@ -37,8 +37,14 @@ end
 
 def isogram?(string)
   original_length = string.length
-  string_array = string.downcase.split
+  p original_length
+
+  string_array = string.downcase.split("")
+  p string_array
+
   unique_length = string_array.uniq.length
+  puts unique_length
+  
   original_length == unique_length
 end
 
