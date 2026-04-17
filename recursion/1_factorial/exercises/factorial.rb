35878012badb3445ef085c37c457641a83afb040
Natalie-B-Wiese <natalie.b.wiese@gmail.com>
2026-04-17 12:45:38 -0400

    Implement recursive factorial method

--- recursion/1_factorial/exercises/factorial.rb
+++ recursion/1_factorial/exercises/factorial.rb
@@ -1,3 +1,10 @@
 def factorial(num)
   # write a recursive method for calculating the [factorial](https://simple.wikipedia.org/wiki/Factorial) of a number
+  if (num<0)
+    return nil
+  elsif (num==1||num==0)
+    return 1
+  else
+    return num * factorial(num-1)
+  end
 end
